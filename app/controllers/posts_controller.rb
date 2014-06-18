2ad4025a7435b02096dd7177338a3f3df8451870
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-06-18 15:16:47 -0600

    very nice how much?

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,33 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
-  def newedit
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  resources :posts
 
-  get 'posts/show'
+  get 'static/about'
 
-  get 'posts/newedit'
+
+  root 'static#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
